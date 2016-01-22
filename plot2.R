d92688b4bf404220b5d911d5a6958361cba29151
Ted Darling <ted@digitalspirittech.com>
2016-01-22 09:34:17 -0600

    Plot the second graph to look like the assignment graph

--- plot2.R
+++ plot2.R
@@ -9,4 +9,19 @@ plot2 <- function(data = NULL){
         ## Get our data set from online.
         data = getData()
     }
+    
+    ## Set PNG Values as per instructions in the assignment.
+    png("plot2.png", width = 480, height = 480)
+    
+    ## Create a histograph to look similar to plot 1 in the assignment.
+    plot(
+        data$DateTime, 
+        data$Global_active_power, 
+        type = "l", 
+        xlab = "", 
+        ylab = "Global Active Power (kilowatts)")
+
+    
+    ## Get off the PNG graphics device.
+    dev.off()
 }
